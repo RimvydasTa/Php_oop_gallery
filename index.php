251f2b0cf5b431dc43f0944e6d5bd80c24ca0660
Rimvydas Tamošiūnas <rimvydas.tamosiunas@unishop.lt>
2018-05-17 11:46:26 +0300

    compress index

--- index.php
+++ index.php
@@ -2,29 +2,15 @@
 
 
         <div class="row">
-
             <!-- Blog Entries Column -->
             <div class="col-md-8">
-
-    
-            
-          
-         
-
             </div>
-
-
-
-
             <!-- Blog Sidebar Widgets Column -->
             <div class="col-md-4">
 
             
-                 <?php include("includes/sidebar.php"); ?>
-
-
+             <?php include("includes/sidebar.php"); ?>
 
         </div>
         <!-- /.row -->
-
         <?php include("includes/footer.php"); ?>
